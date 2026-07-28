4f543ad7d0bb7aa50c85dd315a7f1b67840bf0d3
Sakre Eramani <sakreeramani@gmail.com>
2026-07-28 15:56:28 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,11 @@
+#IDLE is color Coding environment
+name = 'Eramani'
+Branch@ = 'ECE'
+print(Branch@)
+name = 'Eramani'
+print(name)
+
+name,location,age = 'Eramani,Hyderabad,23
+print(name)
+print(location)
+print(age)
